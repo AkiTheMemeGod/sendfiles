6ee7b3b2e969bb946fdb42779b91dbc3184512d0
AkiTheMemeGod <k.akashkumar@gmail.com>
2024-10-24 20:19:10 +0530

    removing the header

--- Dashboard.py
+++ Dashboard.py
@@ -1,5 +1,17 @@
 from assets import *
 
+
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    .stToolbar {visibility: hidden;}
+    .stApp {margin-top: -40px;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 center_title("center", 90, "Black", "Send-Files")
 center_title("s", 70,"green","<br>")
 
